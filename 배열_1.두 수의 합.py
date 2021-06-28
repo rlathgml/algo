be31028beb25bbe689839b0ce95ae2501abc08fc
김소희 <shttr6024@naver.com>
2021-06-28 22:43:13 +0900

    20210628 파이썬 알고리즘 연습

--- 배열_1.두 수의 합.py
+++ 배열_1.두 수의 합.py
@@ -26,11 +26,12 @@ def two_sum2(nums: List[int], target: int) -> List[int]:
     for i, n in enumerate(nums):
         complement = target - n
 
-        if complement in nums[i+1 :]:
+    if complement in nums[i+1 :]:
             return [i, nums[i+1:].index(complement) + (i+1)]
 
 print("in을 이용한 탐색 : ", end=" ")
 print(two_sum2(nums, target))
+
 
 
 # 첫번째 수를 뺀 결과 키 조회
@@ -40,3 +41,13 @@ def two_sum3(nums: List[int], target: int) -> List[int]:
 
     #키와 값을 바꿔서 딕셔너리로 저장
     for i, num in enumerate(nums):
+        nums_map[num] = i
+
+    # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+    for i, num in enumerate(nums):
+        if target-num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target-num]]
+
+
+print("첫번째 수를 뺀 결과 키 조회를 이용한 탐색 : ", end=" ")
+print(two_sum3(nums, target))
